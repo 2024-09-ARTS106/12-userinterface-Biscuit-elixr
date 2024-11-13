7d6565fefb289984049e1f0d78ae214435ca49c8
Biscuit-elixr <vasquezben24@gmail.com>
2024-11-13 12:31:35 -0500

    Setting dialog and close button

--- Assets/LoadScene.cs
+++ Assets/LoadScene.cs
@@ -5,6 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+
     // Start is called before the first frame update
     public void LoadNewScene(string sceneName)
     {
