c993d8659e763db830c4e4bd97fe5c024a2722da
Biscuit-elixr <vasquezben24@gmail.com>
2024-11-13 11:49:33 -0500

    Finished Part 1

--- Assets/LoadScene.cs
+++ Assets/LoadScene.cs
@@ -8,6 +8,6 @@ public class LoadScene : MonoBehaviour
     // Start is called before the first frame update
     public void LoadNewScene(string sceneName)
     {
-
+        SceneManager.LoadScene(sceneName); 
     }
 }
